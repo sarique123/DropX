cc10509d6b4ce0b12a241d252e1c17c75feb9bea
Sarique <sarique20mh@gmail.com>
2025-08-21 10:36:01 +0530

    Day1 : Initial project structure with backend, client, server

---
+++ src/test/java/org/example/dropx/DropXApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dropx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
